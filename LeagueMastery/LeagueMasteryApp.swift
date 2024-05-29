aed08c2e2b4a701930877e02e3c7a8b9c0112235
LeoCapontes <leo8capontes@hotmail.co.uk>
2024-05-29 04:23:33 +0100

    Initial Commit

---
+++ LeagueMastery/LeagueMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueMasteryApp.swift
+//  LeagueMastery
+//
+//  Created by Leo Capontes on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LeagueMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
