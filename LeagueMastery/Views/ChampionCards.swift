fcb9cfcb3f984fc5a8405294495d43bce446a73d
LeoCapontes <leo8capontes@hotmail.co.uk>
2024-07-10 04:11:50 +0100

    Improve champion card. Add medium card.

--- LeagueMastery/Views/ChampionCards.swift
+++ LeagueMastery/Views/ChampionCards.swift
@@ -27,33 +27,88 @@ struct MasteryCrestImage: View {
 
 struct ChampionImage: View {
     var championId: Int
+    var blurred: Bool
     
     var body: some View {
-        AsyncImage(url: URL(string: ChampionIdConversions.init().urlsFromChampId[championId] ?? "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg"))
-        { image in image
-                .image?
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .clipped()
+        ZStack{
+            AsyncImage(url: URL(string: ChampionIdConversions.init().urlsFromChampId[championId] ?? "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg"))
+            { image in image
+                    .image?
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .blur(radius: (blurred ? 10 : 0))
+            }
+            .frame(height: .infinity)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8))
         }
-        .frame(width: 200, height: 200)
     }
 }
 
 struct LargeChampionCard: View {
     var masteryLevel: Int
+    var points: Int
+    var championId: Int
+    
+    var body: some View {
+        ZStack(){
+            HStack{
+                ChampionImage(championId: championId, blurred: true)
+            }
+            HStack{
+                MasteryCrestImage(masteryLevel: masteryLevel)
+                ChampionImage(championId: championId, blurred: false)
+            }
+            .frame(width: .infinity, height: .infinity)
+            HStack {
+                VStack{
+                    Spacer()
+                    Spacer()
+                    Text("  Points: \(points)  ")
+                        .background(.ultraThinMaterial)
+                        .clipShape(Capsule())
+                }
+                Spacer()
+            }
+            .padding(EdgeInsets(top: 0, leading: 28, bottom: 24, trailing: 0))
+        }
+        .frame(width: .infinity ,height: 200)
+        .background(.ultraThinMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+    }
+}
+
+struct MediumChampionCard: View {
+    var masteryLevel: Int
+    var points: Int
     var championId: Int
     
     var body: some View {
         ZStack(){
             HStack{
                 MasteryCrestImage(masteryLevel: masteryLevel)
-                ChampionImage(championId: 4)
+                ChampionImage(championId: championId, blurred: false)
+            }
+            .frame(width: .infinity, height: .infinity)
+            .padding(12)
+            HStack {
+                Spacer()
+                VStack{
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Text("  Points: \(points)  ")
+                        .background(.ultraThinMaterial)
+                        .clipShape(Capsule())
+                    Spacer()
+                }
+                Spacer()
             }
         }
-        .background(.regularMaterial)
+        .frame(width: .infinity ,height: 124)
+        .background(.ultraThinMaterial)
         .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
-        
+        .padding(1)
     }
 }
 
