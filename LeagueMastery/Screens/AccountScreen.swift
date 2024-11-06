6b82c9c9537c772185974598b6293e985176524b
LeoCapontes <leo8capontes@hotmail.co.uk>
2024-11-06 03:31:36 +0000

    Add ability to sort account screen by level rather than points using a picker (tentative ui).

--- LeagueMastery/Screens/AccountScreen.swift
+++ LeagueMastery/Screens/AccountScreen.swift
@@ -11,38 +11,52 @@ import SwiftUI
 struct AccountScreen: View{
     var masteryData: [MasteryResponse]
     
+    var sortOptions = ["Score", "Level"]
+    @State private var toSortBy = "Score"
+    
     var byLevel: [MasteryResponse] {
-        return masteryData.sorted{$0.championLevel < $1.championLevel}
+        return masteryData.sorted{$0.championLevel > $1.championLevel}
     }
     
     var selectedSort: [MasteryResponse] {
-        //placeholder
-        return masteryData
+        switch toSortBy{
+        case "Score":
+            return masteryData
+        case "Level":
+            return byLevel
+        default:
+            return masteryData
+        }
     }
-    
-    let sortOptions = ["Score", "Level"]
     
     var body: some View {
             ZStack{
-                ScrollView(showsIndicators: false) {
-                    NavigationLink(value: selectedSort[0]){
-                        LargeChampionCard(entry: selectedSort[0])
-                    }
-                    HStack{
-                        ForEach(1..<4){ index in
-                            NavigationLink(value: selectedSort[index]){
-                                MediumChampionCard(entry: selectedSort[index])
+                VStack{
+                    ScrollView(showsIndicators: false) {
+                        NavigationLink(value: selectedSort[0]){
+                            LargeChampionCard(entry: selectedSort[0])
+                        }
+                        HStack{
+                            ForEach(1..<4){ index in
+                                NavigationLink(value: selectedSort[index]){
+                                    MediumChampionCard(entry: selectedSort[index])
+                                }
                             }
                         }
-                    }
-                    VStack{
-                        ForEach(4..<selectedSort.count){index in
-                            NavigationLink(value: selectedSort[index]){
-                                ChampionRow(entry: selectedSort[index])
+                        VStack{
+                            ForEach(4..<selectedSort.count){index in
+                                NavigationLink(value: selectedSort[index]){
+                                    ChampionRow(entry: selectedSort[index])
+                                }
                             }
                         }
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
                     }
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    Picker("Sort by: ", selection: $toSortBy) {
+                        ForEach(sortOptions, id: \.self){ option in
+                            Text(option)
+                        }
+                    }
                 }
             }
             .navigationDestination(for: MasteryResponse.self){ entry in
