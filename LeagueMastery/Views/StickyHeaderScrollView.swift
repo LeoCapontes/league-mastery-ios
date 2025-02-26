a075dd606bd802d07b78f7d3caf0b27fc526b569
LeoCapontes <leo8capontes@hotmail.co.uk>
2025-02-26 16:22:31 +0000

    Refactor and add icon to sticky header view
    Pull the stack containing user information out of the scroll view so it's animation isn't affected by scrolling. This removes unwanted vertical movement of the text.

--- LeagueMastery/Views/StickyHeaderScrollView.swift
+++ LeagueMastery/Views/StickyHeaderScrollView.swift
@@ -18,36 +18,61 @@ struct StickyHeaderScrollView<Content:View>: View {
     var body: some View{
         if #available(iOS 18.0, *) {
             GeometryReader{ geo in
-                ScrollView{
-                    LazyVStack(pinnedViews: [.sectionHeaders]){
-                        Section{
-                            content
-                        } header: {
-                            VStack(){
-                                HStack{
-                                    Text(name)
-                                    Text("#\(tag)")
+                ZStack(alignment: .top){
+                    ScrollView{
+                        LazyVStack(pinnedViews: [.sectionHeaders]){
+                            Section{
+                                content
+                            } header: {
+                                VStack(){
+                                    
                                 }
-                                if !yOffset {Text("Score: Placeholder")}
+                                .font(.system(size: yOffset ? 16: 24))
+                                .frame(
+                                    width: geo.size.width,
+                                    height: headerHeight,
+                                    alignment: yOffset ? .leading: .center)
+                                .padding(.horizontal)
+                                .background(.ultraThinMaterial.opacity(yOffset ? 1.0: 0.0))
+                                .animation(.default, value: headerHeight)
                             }
-                            .font(.system(size: yOffset ? 16: 24))
-                            .frame(
-                                width: geo.size.width,
-                                height: headerHeight,
-                                alignment: yOffset ? .leading: .center)
-                            .padding(.horizontal)
-                            .background(.ultraThinMaterial.opacity(yOffset ? 1.0: 0.0))
-                            .animation(.default, value: headerHeight)
                         }
                     }
+                    .onScrollGeometryChange(for: Bool.self) { geo in
+                        geo.contentOffset.y > headerHeight-10
+                    } action: { oldValue, newValue in
+                        yOffset = newValue
+                        headerHeight = yOffset ? 42 : 64
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    
+                    // workaround animations being affected by scrolling by
+                    // overlaying the text on top of the scroll view rather
+                    // than inside of it
+                    HStack(){
+                        // placeholder summoner icon
+                        if !yOffset {
+                            Image(systemName: "person.fill")
+                                .frame(width: 56, height: 56)
+                                .background(.gray)
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                        }
+                        VStack(alignment:.leading){
+                            HStack{
+                                Text(name)
+                                Text("#\(tag)")
+                            }
+                            if !yOffset {Text("Score: Placeholder")}
+                        }
+                    }
+                    .padding(.horizontal)
+                    .frame(
+                        width: geo.size.width,
+                        height: headerHeight,
+                        alignment: .leading
+                    )
+                    .animation(.default, value: headerHeight)
                 }
-                .onScrollGeometryChange(for: Bool.self) { geo in
-                    geo.contentOffset.y > headerHeight-10
-                } action: { oldValue, newValue in
-                    yOffset = newValue
-                    headerHeight = yOffset ? 36 : 56
-                }
-                .clipShape(RoundedRectangle(cornerRadius: 12))
             }
         } else {
             // TODO: Fallback on earlier versions
