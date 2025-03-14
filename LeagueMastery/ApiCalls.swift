2a40007f76cf369e5b7f4e473c64fa2728d359c0
LeoCapontes <leo8capontes@hotmail.co.uk>
2025-03-14 02:43:36 +0000

    Add data pretty print for better debugging.

--- LeagueMastery/ApiCalls.swift
+++ LeagueMastery/ApiCalls.swift
@@ -27,6 +27,7 @@ func puuidApiCall(
     do {
         print(url.absoluteString)
         let (data, _) = try await URLSession.shared.data(from: url)
+        print(data.prettyPrintedJSONString)
         let response = try JSONDecoder().decode(PuuidResponse.self, from: data)
         return response
     } catch {
@@ -72,3 +73,15 @@ func versionApiCall() async throws -> [String]{
     let response = try JSONDecoder().decode([String].self, from: data)
     return response
 }
+
+// From: https://gist.github.com/cprovatas/5c9f51813bc784ef1d7fcbfb89de74fe
+extension Data {
+    var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
+
