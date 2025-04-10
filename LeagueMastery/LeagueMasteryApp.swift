b3f43f228cbf523a9933064d7934a4a11fbd7ed5
LeoCapontes <leo8capontes@hotmail.co.uk>
2025-04-10 19:53:39 +0100

    Add int extension for compacting large numbers.

--- LeagueMastery/LeagueMasteryApp.swift
+++ LeagueMastery/LeagueMasteryApp.swift
@@ -35,3 +35,36 @@ extension Bool: Comparable {
     }
 }
 
+extension Int {
+    // returns a string with the number in a compact format.
+    // e.g. 100,000 -> 100K, 1,000,000 -> 1M
+    // supports numbers under 1 trillion
+    // doesn't support negative numbers
+    func compact() -> String {
+        if (self < 1000) {
+            return String(self)
+        }
+        let formatter = NumberFormatter()
+        formatter.usesSignificantDigits = true
+        formatter.maximumSignificantDigits = 3
+        if (self < 1000000) {
+            var x: Float = Float(self)
+            x = x/1000
+            let number = NSNumber(value: x)
+            return "\(formatter.string(from: number)!)K"
+        }
+        if (self < 1000000000) {
+            var x: Float = Float(self)
+            x = x/1000000
+            let number = NSNumber(value: x)
+            return "\(formatter.string(from: number)!)M"
+        }
+        if (self < 1000000000000) {
+            var x: Float = Float(self)
+            x = x/1000000000
+            let number = NSNumber(value: x)
+            return "\(formatter.string(from: number)!)B"
+        }
+        return String(self)
+    }
+}
