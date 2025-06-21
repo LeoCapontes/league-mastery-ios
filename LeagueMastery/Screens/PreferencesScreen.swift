c0efc23804b78b38e7545bd941864843628b3738
LeoCapontes <leo8capontes@hotmail.co.uk>
2025-06-21 14:01:37 +0100

    Add ability to clear image cache manually.

--- LeagueMastery/Screens/PreferencesScreen.swift
+++ LeagueMastery/Screens/PreferencesScreen.swift
@@ -5,6 +5,7 @@
 //  Created by Leo Capontes on 18/06/2025.
 //
 import SwiftUI
+import Kingfisher
 
 struct PreferencesScreen: View {
     @AppStorage("LevelUpIndicator")
@@ -17,8 +18,20 @@ struct PreferencesScreen: View {
                     Text(mode.rawValue)
                 }
             }
+            Section(footer: Text("Clears saved champion splash arts, useful for when a champion's splash art gets updated")){
+                Button(action: clearImageCache){
+                    Text("Clear image cache")
+                }
+            }
         }
+        .navigationTitle(Text("Settings"))
     }
+}
+
+func clearImageCache() {
+    let cache = ImageCache.default
+    cache.clearMemoryCache()
+    cache.clearDiskCache()
 }
 
 enum LevelUpIndicatorMode: String, CaseIterable, Identifiable {
